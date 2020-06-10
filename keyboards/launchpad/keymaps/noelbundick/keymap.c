e54b0f6519795bae989c457e9d1dead771e3a8e7
Noel Bundick <noelbundick@gmail.com>
2020-06-10 13:01:22 -0700

    configure launchpad for tapdance

--- keyboards/launchpad/keymaps/noelbundick/keymap.c
+++ keyboards/launchpad/keymaps/noelbundick/keymap.c
@@ -1,6 +1,19 @@
 #include QMK_KEYBOARD_H
 #include "noelbundick.h"
 
+// Tap Dance configuration
+enum {
+  TD_LOCK_SLEEP = 0,
+  TD_TASKMGR_CALTDEL
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for lock, twice for sleep
+  [TD_LOCK_SLEEP] = ACTION_TAP_DANCE_DOUBLE(LOCK, KC_SLEP),
+  // Once for task manager, twice for ctrl+alt+del
+  [TD_TASKMGR_CALTDEL] = ACTION_TAP_DANCE_DOUBLE(TASKMGR, CALTDEL)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
@@ -11,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_FN] = LAYOUT(
-  LOCK,    KC_SLEP,
+  _______, TD(TD_TASKMGR_CALTDEL),
   _______, _______,
   _______, _______,
-  _______, TASKMGR
+  _______, TD(TD_LOCK_SLEEP)
 ),
 
 [_KB] = LAYOUT(
