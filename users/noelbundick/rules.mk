4657dc13f79d0da1776c76bb043e3895db7727b8
Noel Bundick <noelbundick@gmail.com>
2020-06-23 19:10:20 -0700

    enable console

--- users/noelbundick/rules.mk
+++ users/noelbundick/rules.mk
@@ -1,4 +1,5 @@
 SRC += noelbundick.c
 
 COMMAND_ENABLE   	= no        # Commands for debug and configuration
+CONSOLE_ENABLE    = yes       # Console for debug
 MOUSEKEY_ENABLE  	= no				# Mouse keys
